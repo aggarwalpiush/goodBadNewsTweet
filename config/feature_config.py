1423a0a05e28aca13f85e81b0e7035eda9305a88
aggarwalpiush <aggarwalpiush@gmail.com>
2019-07-27 13:33:26 +0200

    make config file modular

--- config/feature_config.py
+++ config/feature_config.py
@@ -1,14 +1,15 @@
 ## WARNING: PLEASE TAKE CARE OF TEST OUTPUT PATH, CHANGE IF YOU RUNNING THE MODDELS IF NECESSARY
+GITHUB_PATH = ''
 
-DATA_PATH = '/Users/aggarwalpiush/github_repos/tweetnewsdetection/data/step_one/classification_data/text'
+DATA_PATH = GITHUB_PATH + '/data/step_one/classification_data/text'
 
-LABELS_PATH = '/Users/aggarwalpiush/github_repos/tweetnewsdetection/data/step_one/classification_data/labels/labels.txt'
+LABELS_PATH = GITHUB_PATH +' /data/step_one/classification_data/labels/labels.txt'
 
 TEXT_TYPE = 'text_newsnotnews_natdisaster_mix'
 
 TEST_DOMAIN_ADAPTATION = True
 
-DATA_DOMAIN_PATH = '/Users/aggarwalpiush/github_repos/tweetnewsdetection/data/step_one/classification_data/domain_dataset/natdisaster_mix'
+DATA_DOMAIN_PATH = GITHUB_PATH +'/data/step_one/classification_data/domain_dataset/natdisaster_mix'
 
 TOP_N_TERMS = 300
 
@@ -16,11 +17,11 @@ TOP_N_GRAMS = 2
 
 
 
-TERMS_FILE_PATH = '/Users/aggarwalpiush/github_repos/tweetnewsdetection/intermediate'
+TERMS_FILE_PATH = GITHUB_PATH +'/intermediate'
 
-CHARACTERSTICS_PATH = '/Users/aggarwalpiush/github_repos/tweetnewsdetection/intermediate/characterstics'
+CHARACTERSTICS_PATH = GITHUB_PATH +'/intermediate/characterstics'
 
-FEATURE_PATH = '/Users/aggarwalpiush/github_repos/tweetnewsdetection/feature_vectors'
+FEATURE_PATH = GITHUB_PATH +'/feature_vectors'
 
 ###################################################
 # PLEASE change following path before running main#
@@ -28,19 +29,19 @@ FEATURE_PATH = '/Users/aggarwalpiush/github_repos/tweetnewsdetection/feature_vec
 #												  #
 ###################################################
 
-TEST_OUTPUT_PATH = '/Users/aggarwalpiush/github_repos/tweetnewsdetection/test_results_newsnotnews_'+TEXT_TYPE
+TEST_OUTPUT_PATH = GITHUB_PATH +'/test_results_newsnotnews_'+TEXT_TYPE
 
-EMBEDDING_PATH = '/Users/aggarwalpiush/github_repos/tweetnewsdetection/intermediate/crawl-300d-2M.vec'
+EMBEDDING_PATH = GITHUB_PATH +'/intermediate/crawl-300d-2M.vec'
 
-EMOJI_PATH = '/Users/aggarwalpiush/github_repos/tweetnewsdetection/intermediate/sig_emoji_vector_newsnotnews_'+TEXT_TYPE+'.npy'
+EMOJI_PATH = GITHUB_PATH +'/intermediate/sig_emoji_vector_newsnotnews_'+TEXT_TYPE+'.npy'
 
-POSITIVE_INTERJ_FILE = '/Users/aggarwalpiush/github_repos/tweetnewsdetection/intermediate/positive-interjections.txt'
+POSITIVE_INTERJ_FILE = GITHUB_PATH +'/intermediate/positive-interjections.txt'
 
-NEGATIVE_INTERJ_FILE = '/Users/aggarwalpiush/github_repos/tweetnewsdetection/intermediate/negative-interjections.txt'
+NEGATIVE_INTERJ_FILE = GITHUB_PATH +'/intermediate/negative-interjections.txt'
 
-POSITIVE_LEXICON_FILE = '/Users/aggarwalpiush/github_repos/tweetnewsdetection/intermediate/positive-words.txt'
+POSITIVE_LEXICON_FILE = GITHUB_PATH +'/intermediate/positive-words.txt'
 
-NEGATIVE_LEXICON_FILE = '/Users/aggarwalpiush/github_repos/tweetnewsdetection/intermediate/negative-words.txt'
+NEGATIVE_LEXICON_FILE = GITHUB_PATH +'/intermediate/negative-words.txt'
 
 
 TF_IDF_MAX_FEATURES = 300
